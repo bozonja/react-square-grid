57d6ea4354e1631b149369f1cf3f2e56ecbc6c75
Josip Markovic <bozadesign@gmail.com>
2022-08-23 15:25:03 +0200

    Add gird with rows and random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import "./App.css";
-import { Square } from "./components/Square/Square";
+//components
+import { Rows } from "./components/Rows";
 
 function App() {
   const size = {
-    sm: {
-      width: "20px",
-      height: "20px",
-    },
-    lg: {
-      width: "50px",
-      height: "50px",
-    },
+    sm: "20px",
+    md: "35px",
+    lg: "50px",
   };
 
+  // "Max amount of cols or rows is 10!
+  const numberOfCols = 10;
+  const numberOfRows = 10;
+  const numberOfColsAndRows = numberOfCols * numberOfRows;
+
+  const cols = new Array(numberOfCols).fill(0);
+  const rows = new Array(numberOfRows).fill(0);
+
+  if (numberOfCols > 10 || numberOfRows > 10) {
+    throw new Error(
+      "Max amount of cols or rows is 10! Total number of squares must not exceed 99!"
+    );
+  }
+
   return (
-    <div className="App">
-      <Square size={size.sm} />
+    <div>
+      <div className="grid">
+        {cols.map((_, i) => (
+          <div key={i}>
+            {rows.map((_, i) => (
+              <Rows
+                key={i}
+                size={size.md}
+                numberOfColsAndRows={numberOfColsAndRows}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {/* //TODO: change sizes by cliking buttons */}
+      <p>Choose field size:</p>
+      <button onClick={() => {}}>Sm</button>
+      <button onClick={() => {}}>Md</button>
+      <button onClick={() => {}}>Lg</button>
     </div>
   );
 }
