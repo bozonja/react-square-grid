1f90c7db872766e62612fb332da52437b8dae4d0
Josip Markovic <bozadesign@gmail.com>
2022-08-24 12:31:05 +0200

    Add error to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+//styles
 import "./App.css";
 //components
 import { Rows } from "./components/Rows";
@@ -8,6 +9,8 @@ function App() {
     md: "35px",
     lg: "50px",
   };
+  const errorMessage =
+    "Oops...Max amount of cols or rows is 10! Total number of fields must not exceed 99!";
 
   // "Max amount of cols or rows is 10!
   const numberOfCols = 10;
@@ -17,14 +20,23 @@ function App() {
   const cols = new Array(numberOfCols).fill(0);
   const rows = new Array(numberOfRows).fill(0);
 
-  if (numberOfCols > 10 || numberOfRows > 10) {
-    throw new Error(
-      "Max amount of cols or rows is 10! Total number of squares must not exceed 99!"
-    );
+  const checkNoOfCols = () => {
+    if (numberOfCols > 10 || numberOfRows > 10) {
+      throw new Error(errorMessage);
+    }
+  };
+
+  try {
+    checkNoOfCols();
+  } catch (e) {
+    return <h3 style={{ color: "red" }}>{errorMessage}</h3>;
   }
+
+  // Vrijednosti u poljima se ne smiju ponavljati. Ukupan broj polja je ograničen na 99.
 
   return (
     <div>
+      <p>Click on field to get random number:</p>
       <div className="grid">
         {cols.map((_, i) => (
           <div key={i}>
@@ -38,7 +50,6 @@ function App() {
           </div>
         ))}
       </div>
-
       {/* //TODO: change sizes by cliking buttons */}
       <p>Choose field size:</p>
       <button onClick={() => {}}>Sm</button>
